2d013333137bc07e2876c7e12d0f0b93ff6c7fcb
Travis Peters <traviswp@gmail.com>
2020-08-23 12:31:38 -0600

    cleaning up example

--- week01/100000-beers/beer-process.c
+++ week01/100000-beers/beer-process.c
@@ -4,13 +4,10 @@
 static void PrintMessage(int i);
 static void GoToSleep(void);
 
-
-int main(void)
-{
+int main(void) {
 	int i = 100000;
 
-	while ( 1 )
-	{
+	while ( 1 ) {
 		PrintMessage( i );
 		GoToSleep();
 		i -= 1;
@@ -19,16 +16,10 @@ int main(void)
 	return 0;
 }
 
-
-
-void PrintMessage(int i)
-{
-		printf("%d bottles of beer on the wall.\n", i);
+static void PrintMessage(int i) {
+	printf("%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around...\n", i, i);
 }
 
-
-
-static void GoToSleep(void)
-{
-	sleep(3);
+static void GoToSleep(void) {
+	sleep(1);
 }
