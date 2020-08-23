80e20d2b942cd3b6e6ff8c19ab2ab25a003d0a8e
Travis Peters <traviswp@gmail.com>
2020-08-23 12:32:59 -0600

    cleaning up example

--- week01/csci112-extended/secret.c
+++ week01/csci112-extended/secret.c
@@ -1,13 +1,24 @@
+/*
+ * Encrypt (encode) plain text into something unintelligible.
+ * This program will read in a file of text and map each letter to another letter using the following array:
+ *  char key[27] = “efghijklmnopqrstuvwxyzabcd”;
+ * This means that if you encounter the letter ‘a’, then you will replace it with the letter ‘e’, etc.
+ * If you encounter any other characters in the input file (example: ‘A’ or space, etc), write them as is.
+ *
+ * Write each line of text (unencrypted and encrypted) to another text file.
+ *
+ * Adapted from CSCI 112 @ Montana State University (thanks, Mary Ann Cummings!).
+ */
+
 #include <stdio.h>
 #include <string.h>
 
 #define MAX_LINE_SIZE 100
 
 void encode(char* line, FILE* optr) {
-
     char key[127] = "efghijklmnopqrstuvwxyzabcd";
 
-    for (int i = 0; i < strlen(line); ++i) {
+    for (unsigned int i = 0; i < strlen(line); ++i) {
         int ascii_place = line[i];
         if (97 <= ascii_place && ascii_place <= 122) {
             fprintf(optr, "%c", key[ascii_place-97]);
@@ -20,7 +31,6 @@ void encode(char* line, FILE* optr) {
 }
 
 int main(void) {
-
     FILE* infptr = fopen("./tinyTale.txt", "r");
     FILE* outfptr = fopen("out.txt", "w");
 
